1a340f28ccf42b383845196195e37591413d7176
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-03-20 10:38:58 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DailyMood::Application.config.secret_key_base = '305b644d254e01dac9128a8377a94c8f1f9ea378e1a183c8ad7b4e2f135a8c9ddef44344b3c695047ed0ea0107ac0a706c9ec49d03e0f9dd007c948e21ca5cb1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DailyMood::Application.config.session_store :cookie_store, key: '_daily_mood_session'
